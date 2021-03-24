f37576b6baa7c21d88aa211fe330313a5b6ac358
neillschiks@hotmail.com <neillschiks@hotmail.com>
2021-03-24 20:45:24 +0100

    End of day 3

--- Frontend/src/app/services/read-file.service.ts
+++ Frontend/src/app/services/read-file.service.ts
@@ -6,78 +6,101 @@ import { CourseInstance } from '../models/CourseInstance';
   providedIn: 'root'
 })
 export class ReadFileService {
+  data: string;
 
   constructor() { }
 
-  readThis(inputValue: any): CourseInstance[] {
+  loadFile(inputValue) {
     let file: File = inputValue.files[0];
-    let myReader: FileReader = new FileReader();
-    let courses: CourseInstance[] = [];
-    //var fileType = inputValue.parentElement.id;
-    myReader.onloadend = function(e) {
-      //myReader.result is a String of the uploaded file
-      let data = myReader.result as string;
-      let array = data.split('\n\n');
+    let reader: FileReader = new FileReader();
 
-      let document: any[] = [];
-      for(let i = 0; i < array.length; i++) {
-        if(array[i] !== ''){
-          let object = (array[i].split('\n'));
-          document.push(object);
-        }
-      }
-        
-        
-      for(let i = 0; i < document.length; i++){
-        var object = document[i];
-        let Title: string;
-        let Duration: string;
-        let Code: string;
-        let DateStart: Date;
-        for(let i = 0; i < object.length; i++){
-          let t: string[] = object[i].split(':');
+    reader.onloadend = ((e) => {
+      this.data = reader.result as string;
+    });
+    reader.readAsText(file);
+  }
 
-          switch(t[0]) { 
-            case 'Titel': { 
-              Title = t[1].trim();
-              break; 
-            } 
-            case 'Duur': { 
-              Duration = t[1].trim();
-              break; 
-            } 
-            case 'Cursuscode': { 
-              Code = t[1].trim();
-              break; 
-            } 
-            case 'Startdatum': { 
-              let dateString = t[1].trim();
-              let dateParts = dateString.split("/");
-              var dateObject = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
-              DateStart = new Date(dateObject);
-              break; 
-            } 
-            default: { 
-              console.error(`${t[0]}: is niet toegestaan`);
-              break; 
-            } 
+  check(inputValue: any): boolean{
+    
+
+
+    return true;
+  }
+
+
+
+  readThis(inputValue: any): CourseInstance[] {
+    if(this.check(inputValue) === true)
+    {
+      let file: File = inputValue.files[0];
+      let myReader: FileReader = new FileReader();
+      let courses: CourseInstance[] = [];
+    
+      myReader.onloadend = function(e) {
+        let data = myReader.result as string;
+        let array: string[] = data.split('\n\n');
+
+        let document: any[] = [];
+        for(let i = 0; i < array.length; i++) {
+          if(array[i] !== ''){
+            let object = (array[i].split('\n'));
+            document.push(object);
           }
         }
-        let courseTemp: Course = {
-          title: Title,
-          duration: Duration,
-          code: Code
-        };
+        
+        
+        for(let i = 0; i < document.length; i++){
+          var object = document[i];
+          let Title: string;
+          let Duration: string;
+          let Code: string;
+          let DateStart: Date;
+          for(let i = 0; i < object.length; i++){
+            let t: string[] = object[i].split(':');
 
-        let courseInstance: CourseInstance = {
-          startDate: DateStart,
-          course: courseTemp 
-        };
-        courses.push(courseInstance);
+            switch(t[0]) { 
+              case 'Titel': { 
+                Title = t[1].trim();
+                break; 
+              } 
+              case 'Duur': { 
+                Duration = t[1].trim();
+                break; 
+              } 
+              case 'Cursuscode': { 
+                Code = t[1].trim();
+                break; 
+              } 
+              case 'Startdatum': { 
+                let dateString = t[1].trim();
+                let dateParts = dateString.split("/");
+                var dateObject = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+                DateStart = new Date(dateObject);
+                break; 
+              } 
+              default: { 
+                console.error(`${t[0]}: is niet toegestaan`);
+                break; 
+              } 
+            }
+          }
+          
+          let courseTemp: Course = {
+            title: Title,
+            duration: Duration,
+            code: Code
+          };
+
+          let courseInstance: CourseInstance = {
+            startDate: DateStart,
+            course: courseTemp 
+          };
+          courses.push(courseInstance);
+        }
+        console.log(courses);
       }
-      console.log(courses);
+      myReader.readAsText(file);
+      return courses;
+      }
     }
-    myReader.readAsText(file);
-    return courses;
-  }
 }
